232d30cf95ce85bee41de7bb2d1f1e48c6b7453c
jo87jimmy <jo87jimmy@yahoo.com.tw>
2021-12-15 11:40:06 +0800

    origin

---
+++ Dictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<int,string> dict = new Dictionary<int, string>();
+
+           dict.Add(0,"Jeff");
+           dict.Add(1,"Jimmy");
+           dict.Add(2,"Terry");
+
+           Console.WriteLine(dict[0]);
+           Console.WriteLine(dict[0][2]);
+           Console.WriteLine(dict.Count);
+        }
+    }
+}
